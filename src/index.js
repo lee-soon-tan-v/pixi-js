4e310abc39c4af849d3e8eb54b871fc146989d01
lee-soon-tan-v <lee-soon.tan@vitrox.com>
2025-12-16 15:35:11 +0800

    Simplify index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,12 @@
-import { Application, Assets, Sprite } from 'pixi.js';
+import * as PIXI from 'pixi.js';
 
-(async () => {
-  // Create a PixiJS application
-  const app = new Application();
-  await app.init({ width: window.innerWidth, height: window.innerHeight, backgroundColor: 0x1099bb });
-  document.body.appendChild(app.canvas);
+console.log(PIXI.Application); // Should log a function
 
-  // Load a texture and create a sprite
-  const texture = await Assets.load('https://pixijs.com/assets/bunny.png');
-  const bunny = new Sprite(texture);
-  bunny.anchor.set(0.5);
-  bunny.x = app.screen.width / 2;
-  bunny.y = app.screen.height / 2;
-  app.stage.addChild(bunny);
+class MyApp extends PIXI.Application {
+  constructor() {
+    super({ width: 800, height: 600 });
+  }
+}
 
-  // Animate the bunny
-  app.ticker.add((time) => {
-    bunny.rotation += 0.1 * time.deltaTime;
-  });
-})();
+const app = new MyApp();
+document.body.appendChild(app.canvas);
