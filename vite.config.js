871a361bd03e4e66a8967c466fb7fc0cfc15989c
lee-soon-tan-v <lee-soon.tan@vitrox.com>
2025-12-16 16:15:33 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  root: "src", // tells Vite that src/ is the project root
   server: {
     host: true,
     allowedHosts: "all"
